f72d5ac1889b8c33d5978ca96b73767a1c0156c2
ASKqqq <akalinin33@gmail.com>
2022-12-17 22:59:34 +0500

    Add style.css and display of cats

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-console.log('fffcdcf')
+const $wrapper = document.querySelector('[data-wrapper]')
 
+const getCat = (cat) => `
+<div class="card" style="width: 18rem;">
+  <img src="${cat.image}" class="card-img-top" alt="${cat.name}">
+  <div class="card-body">
+    <h5 class="card-title">${cat.name}</h5>
+    <p class="card-text">${cat.description}.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+`
 
-
-
+fetch('https://cats.petiteweb.dev/api/single/ASKqqq/show/')
+  .then((res) => res.json())
+  .then((data) => $wrapper.insertAdjacentHTML('afterbegin', data.map((cat) => getCat(cat)).join('')))
